d2a8176d98bed80cf178bdc4aafefcfbd6ee00f0
gergohrubo <gergohrubo@gmail.com>
2019-12-03 17:06:00 +0100

    Database setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:secret@localhost:5434/postgres');
+
+const User = sequelize.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  }
+});
+
+const Task = sequelize.define('task', {
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  completed: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+})
+
+sequelize.sync()
+  .then(() => console.log('Tables created successfully'))
+  .catch(err => {
+    console.error('Unable to create tables, shutting down...', err);
+    process.exit(1);
+  })
